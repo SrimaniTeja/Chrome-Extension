6b9c804d4b5f5bb04f076f2a3e6947b288c63212
Srimani Teja Ejjagiri <123293913+SrimaniTeja@users.noreply.github.com>
2025-01-03 10:59:08 +0530

    s

--- popup.js
+++ popup.js
@@ -1,1 +1,2 @@
-document.getElementById("op").addEventListener("click",()=> window.open("form.html", '', 'width=600,height=400'))
+document.getElementById("op").addEventListener("click",()=> window.open("form.html", '', 
+    'width=800,height=500'))
