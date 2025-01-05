60c2ff62de2705fdc72e76036737ebea2492da2e
VIVEKANANDA <vivekanandareddy56@gmail.com>
2025-01-05 18:04:53 +0530

    api remaining

--- service.js
+++ service.js
@@ -1,34 +1,37 @@
-if (typeof browser === "undefined") {
-    var browser = chrome;
-}
-function reddenPage(url) {
-}
 var a=2
-let dictionary = new Object();
+let dictionary = [];
+let len=0;
+let skipthisshit=false;
 chrome.webRequest.onBeforeRequest.addListener(tab => {
     chrome.scripting.executeScript(
         {
             target: { tabId: tab.tabId },
-            function: reddenPage,
             args: [tab.url],
         },
-        () => { 
-            console.log(tab.url,tab.tabId);
-            if(safe(tab.url)){
-                
-            }else{
-                console.log('exitttttttttttttt',tab.tabId,safe(tab.url))
-                // const executing = chrome.tabs.executeScript(tab.id, {
-                //     code: "window.stop();",
-                //     allFrames: true,
-                //     runAt: "document_start"
-                // });
-                chrome.scripting.executeScript({
-                    target: {tabId: tab.tabId}
-                },
-                ()=>{
-                    chrome.tabs.remove(tab.tabId)
-                });
+        () => {
+            skipthisshit=false
+            for(let cit=0;cit<len;cit++){
+                if(tab.tabId==dictionary[cit]){
+                    skipthisshit=true
+                    break;
+                }
+            }
+            if(skipthisshit==false)
+            {
+                console.log(tab.url,tab.tabId,dictionary);
+                len=dictionary.push(tab.tabId)
+                if(safe(tab.url)){
+                    
+                }else{
+                    console.log('exitttttttttttttt',tab.tabId)
+                    chrome.scripting.executeScript({
+                        target: {tabId: tab.tabId}
+                    },
+                    ()=>{
+                        chrome.tabs.remove(tab.tabId)
+                        console.log(dictionary.pop())
+                    });
+                }
             }
          });
     },  {
